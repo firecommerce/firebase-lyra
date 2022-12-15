3299a981e22d9d777d807836bc619714fdc32521
Leonardo Matos <leomp120894@gmail.com>
2022-12-15 13:44:50 -0300

    Ensure `initializeApp()` call

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,8 @@
+import {initializeApp} from "firebase-admin/app";
 import {onRequest} from "firebase-functions/v2/https";
 import fillDemo from "./demo/fill";
+
+initializeApp();
 
 export const fill = onRequest({
   timeoutSeconds: 1800,
